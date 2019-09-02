e121c055e55f4aac5bbc72828b312e83eea17e57
nikuqhuang <35030601+nikuqhuang@users.noreply.github.com>
2019-09-02 11:15:17 +0800

    Create p3.java

---
+++ P3/p3.java
@@ -0,0 +1,46 @@
+//package hourse;
+
+import java.util.Scanner;
+public class p3 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int x,y;
+		double n;
+		x = scan.nextInt();
+		y = scan.nextInt();
+		n = Math.min(x, y);
+		
+		if((Math.min(x,y)==1)||(Math.min(x,y)==2&&x==y)) {
+			System.out.println("Lose"+" "+"0");
+		}
+		else if(Math.min(x,y)==2||Math.min(x,y)==3) {
+			System.out.println("Win"+" "+"1");
+		}
+		else if(x==y) {
+			if(x%3==0) {
+				x=x/3;
+				x=2*x-1;
+				System.out.println("Win"+" "+x);
+			}
+			else {
+				x=x/3;
+				x=2*x;
+				System.out.println("Lose"+" "+x);
+			}
+		}
+		else if(n%3==1) {
+			n=n/3;
+			n=2*n;
+			System.out.println("Lose"+" "+(int)n);
+		}
+		else {
+			n=Math.ceil(n/3);
+			n=2*n-1;
+			System.out.println("Win"+" "+(int)n);
+		}
+
+		scan.close(); 
+    
+	}
+}
+
