fcfecd498b8e5f11ca5bfc05abfcacbf8fc4047c
nikuqhuang <35030601+nikuqhuang@users.noreply.github.com>
2019-09-02 11:13:47 +0800

    Update p1.java

--- P1/p1.java
+++ P1/p1.java
@@ -1,1 +1,18 @@
+//package fractal;
+
+import java.util.Scanner;
+import java.math.BigInteger;
+public class p1 {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n;
+		
+		n = scan.nextInt();
+		BigInteger bfour = new BigInteger("4");
+		BigInteger bthree = new BigInteger("3");
+		
+		System.out.println(bfour.pow(n).subtract(BigInteger.ONE).divide(bthree));
+		scan.close();
+	}	
+}
 
